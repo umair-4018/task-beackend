927bcbfc43dcbd473b4b9018290ae2a6823b0481
umair <muhammadumair4018@gmail.com>
2024-04-08 11:58:08 +0500

    routes correction

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
-app.get((req,res)=>{
+app.get("/",(req,res)=>{
   res.send("hellow")
 })
 app.listen(port, () => {
